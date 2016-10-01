87b46ee5f3a71dd5696df04d18690b3754feb079
Anthony Li <antli.inbox@gmail.com>
2016-10-01 15:55:00 -0400

    Add initial models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    filename = models.CharField(max_length=200)
+    votes = models.ManyToManyField(Vote)
+    note_sequence = models.CharField(max_field=200)
+
+class Vote(models.Model):
+    score = models.IntegerField(default=0)
