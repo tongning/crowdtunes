9a427a62a9199d7022d5a73e052feddac5ec8be4
Angel <angel.m.wen@gmail.com>
2016-10-01 19:04:16 -0400

    Merge branch 'master' of https://github.com/tongning/crowdtunes
    # Conflicts:
    #	.idea/workspace.xml
    snothing much

--- core/models.py
+++ core/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Vote(models.Model):
     score = models.IntegerField(default=0)
 
+class SongManager(models.Manager):
+    def create_song(self, filename, note_sequence):
+        song = self.create(filename=filename,note_sequence=note_sequence)
+        return song
 class Song(models.Model):
+
     filename = models.CharField(max_length=200)
     votes = models.ManyToManyField(Vote)
     note_sequence = models.CharField(max_length=200)
+    objects = SongManager()
 
 
