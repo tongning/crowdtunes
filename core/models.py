2845014af2bf862f792eb6245d4eacc90bbab1a6
Anthony Li <antli.inbox@gmail.com>
2016-10-01 15:58:39 -0400

    Move vote

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Vote(models.Model):
+    score = models.IntegerField(default=0)
+
 class Song(models.Model):
     filename = models.CharField(max_length=200)
     votes = models.ManyToManyField(Vote)
     note_sequence = models.CharField(max_field=200)
 
-class Vote(models.Model):
-    score = models.IntegerField(default=0)
+
