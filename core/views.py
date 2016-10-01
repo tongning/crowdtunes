d2ac7f5937b182defaf33909a29a6d8a92ab7d54
Anthony Li <antli.inbox@gmail.com>
2016-10-01 16:25:06 -0400

    Autogenerate on homepage load

--- core/views.py
+++ core/views.py
@@ -1,5 +1,61 @@
 from django.shortcuts import render
-
+import os
+import numpy as np
+import glob
+import random
+from pydub import AudioSegment
+import pydub
 # Create your views here.
 def index(request):
-    return render(request, 'index.html' )
+    a = AudioSegment.from_wav("notes/A.wav")
+    b = AudioSegment.from_wav("notes/B.wav")
+    c = AudioSegment.from_wav("notes/C.wav")
+    d = AudioSegment.from_wav("notes/D.wav")
+    e = AudioSegment.from_wav("notes/E.wav")
+    f = AudioSegment.from_wav("notes/F.wav")
+    g = AudioSegment.from_wav("notes/G.wav")
+    asharp = AudioSegment.from_wav("notes/Asharp.wav")  # Bb
+    dsharp = AudioSegment.from_wav("notes/Dsharp.wav")  # Eb
+    csharp = AudioSegment.from_wav("notes/Csharp.wav")
+    fsharp = AudioSegment.from_wav("notes/Fsharp.wav")
+    gsharp = AudioSegment.from_wav("notes/Gsharp.wav")
+    notesString = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'Csharp', 'Dsharp', 'Fsharp', 'Gsharp', 'Asharp']
+    notes = [a, b, c, d, e, f, g, csharp, dsharp, fsharp, gsharp, asharp]
+    possTimes = [125, 250, 500, 750, 1000]
+
+    def makeMelody(numNotes):
+        melody, melodyString = []
+        idx = np.random.choice(np.arange(notes), numNotes, replace=True)
+        melody = notes[idx]
+        melodyString = notesString[idx]
+        return melody, melodyString
+
+    def makeTimes(numNotes):
+        times = []
+        for x in range(0, numNotes):
+            times.append(random.choice(possTimes))
+        return times
+
+    def makeTimedMelody(melody, times):
+        song = melody[0][:times[0]]
+        for i in range(1, len(melody)):
+            song += melody[i][:times[i]]
+        return song
+
+    def exportMelody(melody):
+
+        oldSongs = glob.glob("../crowdtunes/staticfiles/tuneFiles/*.wav")
+        songsNum = len(oldSongs)
+        songcount = songsNum + 1
+        melody.export("../crowdtunes/staticfiles/tuneFiles/tune%d.wav" % songcount, format="wav")
+
+    # Create your views here.
+
+    numNotes = 8
+    melodyNotes, melodyString = makeMelody(numNotes)
+    times = makeTimes(numNotes)
+    timedMelody = makeTimedMelody(melodyNotes, times)
+    exportMelody(timedMelody)
+
+    return render(request, 'index.html')
+
