4138cffb3205e97586ff66122905400641461968
Anthony Li <antli.inbox@gmail.com>
2016-10-01 17:50:30 -0400

    create new static folder

--- crowdtunes/settings.py
+++ crowdtunes/settings.py
@@ -123,8 +123,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+
 
