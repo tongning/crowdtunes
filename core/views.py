57d87b2b49da5cc5c4f5df73b7aff0cdebc4e8e7
AndySebastian <andysebastian8@gmail.com>
2016-10-01 22:46:57 -0400

    play automatically on page load

--- core/views.py
+++ core/views.py
@@ -122,54 +122,7 @@ def combined(request):
         all_songs = Song.objects.all().order_by('averageVote')
         chosen = [all_songs[len(all_songs)-1], all_songs[len(all_songs)-2], all_songs[len(all_songs)-3],
         all_songs[len(all_songs)-4]]
-        return chosen
 
-    def setMelody(chosen):
-        a = AudioSegment.from_wav("core/notes/A.wav")
-        b = AudioSegment.from_wav("core/notes/B.wav")
-        c = AudioSegment.from_wav("core/notes/C.wav")
-        d = AudioSegment.from_wav("core/notes/D.wav")
-        e = AudioSegment.from_wav("core/notes/E.wav")
-        f = AudioSegment.from_wav("core/notes/F.wav")
-        g = AudioSegment.from_wav("core/notes/G.wav")
-        asharp = AudioSegment.from_wav("core/notes/Asharp.wav")  # Bb
-        dsharp = AudioSegment.from_wav("core/notes/Dsharp.wav")  # Eb
-        csharp = AudioSegment.from_wav("core/notes/Csharp.wav")
-        fsharp = AudioSegment.from_wav("core/notes/Fsharp.wav")
-        gsharp = AudioSegment.from_wav("core/notes/Gsharp.wav")
-        notesString = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'C#', 'D#', 'F#', 'G#', 'A#']
-        notes = [a, b, c, d, e, f, g, csharp, dsharp, fsharp, gsharp, asharp]
-        possTimes = [125, 250, 500, 750, 1000]
-        melody = []
-        i = 0
-        for x in chosen:
-            j = 0
-            for y in chosen[i].songMelody:
-                melody.append(chosen[i].songMelody[j])
-                ++j
-            ++i
-        return melody
 
-    def setTimes(numNotes):
-        times = []
-        for x in range(0, numNotes):
-            times.append(random.choice(possTimes))
-        return times
-
-    def setTimedMelody(melody, times):
-        song = melody[0][:times[0]]
-        for i in range(1, len(melody)):
-            song += melody[i][:times[i]]
-        return song
-
-    def setFileName():
-        oldSongs = glob.glob("core/static/tuneFiles/*.wav")
-        songsNum = len(oldSongs)
-        songcount = songsNum + 1
-        return "tune%d.wav" % songcount
-
-    def setExportMelody(melody, fileName):
-        melody.export("core/static/tuneFiles/%s" % fileName, format="wav")
-        melody.export("crowdtunes/staticfiles/%s" % fileName, format="wav")
 
     return render(request, 'combined.html', {'message':'hello!'})
