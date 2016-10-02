edf162960eca21cd831c859468bb89ebf82d004f
AndySebastian <andysebastian8@gmail.com>
2016-10-01 21:34:14 -0400

    play automatically on page load

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^download/(?P<filename>\w+)$', views.download, name='download'),
-    url(r'^/combined/$', views.combined, name='combined')
+    url(r'^combined/$', views.combined, name='combined')
 
 ]
